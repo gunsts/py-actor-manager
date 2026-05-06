56b6edf7b8381c77ed829cdc0c447ae4f2aeeeaa
gunsts <137734013+gunsts@users.noreply.github.com>
2026-05-06 21:07:12 +0700

    Solution

--- app/managers.py
+++ app/managers.py
@@ -3,23 +3,23 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self, db_name: str, table_name: str):
+    def __init__(self, db_name: str, table_name: str) -> None:
         self.db_name = db_name
         self.table_name = table_name
         self.connection = sqlite3.connect(self.db_name)
         self.cursor = self.connection.cursor()
-        
+
         # Ensure the table exists when the manager is initialized
-        self.cursor.execute(f'''
+        self.cursor.execute(f"""
             CREATE TABLE IF NOT EXISTS {self.table_name} (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 first_name TEXT,
                 last_name TEXT
             )
-        ''')
+        """)
         self.connection.commit()
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self.cursor.execute(
             f"INSERT INTO {self.table_name} \
                 (first_name, last_name) VALUES (?, ?)",
@@ -27,14 +27,17 @@ class ActorManager:
         )
         self.connection.commit()
 
-
     def all(self) -> list[Actor]:
-        self.cursor.execute(f"SELECT id, first_name, last_name FROM {self.table_name}")
+        self.cursor.execute(
+            f"SELECT id, first_name, last_name FROM {self.table_name}")
         rows = self.cursor.fetchall()
-        return [Actor(id=row[0], first_name=row[1], last_name=row[2]) for row in rows]
+        return [Actor(id=row[0], first_name=row[1],
+                      last_name=row[2]) for row in rows]
 
-
-    def update(self, pk: int, new_first_name: str, new_last_name: str):
+    def update(self,
+               pk: int,
+               new_first_name: str,
+               new_last_name: str) -> None:
         self.cursor.execute(
             f"UPDATE {self.table_name} SET first_name = ?, \
                 last_name = ? WHERE id = ?",
@@ -42,14 +45,12 @@ class ActorManager:
         )
         self.connection.commit()
 
-
     def delete(self, pk: int) -> None:
         self.cursor.execute(
             f"DELETE FROM {self.table_name} WHERE id = ?", (pk,))
         self.connection.commit()
 
-
     def __del__(self) -> None:
-        # Good practice to close connection 
+        # Good practice to close connection
         # when the object is destroyed
         self.connection.close()
--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Actor:
-    id: str
+    id: int
     first_name: str
     last_name: str
